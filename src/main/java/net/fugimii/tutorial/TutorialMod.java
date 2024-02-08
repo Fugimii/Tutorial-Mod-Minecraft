b2edc2923fe4328e2df873b60ba0d1d73bcdcaac
Fugimii <louisrumbleskins@gmail.com>
2024-02-08 18:51:47 +1000

    initial commit

---
+++ src/main/java/net/fugimii/tutorial/TutorialMod.java
@@ -0,0 +1,23 @@
+package net.fugimii.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fugimii.tutorial.block.ModBlocks;
+import net.fugimii.tutorial.item.ModItemGroups;
+import net.fugimii.tutorial.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
